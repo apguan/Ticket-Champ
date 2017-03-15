c05a334c62799e0afdc8b4da0b62da361b4ed250
samzoll22 <sam.zoll22@gmail.com>
2017-03-15 14:38:40 -0700

    Built out AJAX POST/GET Handling for react client

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -5,14 +5,32 @@ import $ from 'jquery';
 class App extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {}
+    this.state = {
+      value: ""
+    }
   }
 
-// componentDidMount() {
-//   $ajax
-// }
+search (input) {
+  $.ajax({
+      type:"POST",
+      url:"/event",
+      data: input,
+      success: function() {
+        console.log('Post Data Success')
+      }
+  })
+}
 
-
+componentDidMount() {
+  $.ajax({
+      type:"GET",
+      url:"/home",
+      // data: data,
+      success: function() {
+        console.log('Post Data Success')
+      }
+  })
+}
 
 
   render  () {
