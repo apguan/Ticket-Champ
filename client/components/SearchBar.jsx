d63bfe03775f782d28de89806e15a4f9b2464a69
Allen Guan <apguan@gmail.com>
2017-03-15 16:17:13 -0700

    made search a class component

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,8 +1,34 @@
-function Search() {
-  return (
-    //this.state should have a variable that will be set to this value
-    <input type="text" value={this.state.value} placeholder="search up an artist"></input>
-  )
+import React from 'react';
+
+class Search extends react.components {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value = "lol try again"
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  search(props) {
+    this.props.onSearch(this.state.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.value} placeholder="search up an artist" onChange={this.onChange}></input>
+        <button>Compare Tickets</button>
+      </div>
+    )
+  }
 }
 
 export default Search
