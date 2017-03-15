7565871901d1fb66c41f956613d5d6b355bd5b4a
Chris Chu <cchu008@gmail.com>
2017-03-15 15:47:41 -0700

    initial version of Location selector dropdown menu

--- client/components/LocationSelector.jsx
+++ client/components/LocationSelector.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 
+
 // simple rendering for Location
-// add onCli event handler
+// dropdown menu
 
 class Location extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleLocation(event) {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
   render() {
-    return <input type="text" value={this.state.location} placeholder="City"></input>
+    return (
+      <span>Select Location</span>
+      <select value={this.state.value} onChange={this.handleLocation.bind(this)}>
+        <option value="1" active>San Francisco</option>
+        <option value="2">Los Angeles</option>
+        <option value="3">New York</option>
+      </select>
+    );
   }
 }
 
-}
+export default Location;
