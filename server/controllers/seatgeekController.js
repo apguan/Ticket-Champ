8cec726466d27b441d4b5e9d43726dae52517c32
Chris Chu <cchu008@gmail.com>
2017-03-20 11:13:50 -0700

    added location as parameter to SeatGeekController

--- server/controllers/seatgeekController.js
+++ server/controllers/seatgeekController.js
@@ -1,19 +1,14 @@
 var app = require('../index.js');
 var request = require('request');
 
-var seatGeekGetter = function(searchParam) {
-  search = searchParam.split('');
-  for(var i=0; i<search.length; i++) {
-   if (search[i] === ' ') {
-       search.splice(i, 1, '+');
-   }
-  }
+var seatGeekGetter = function(searchParam, location, callback) {
+  search = searchParam.split(' ').join('+');
+
   var queryString = 'https://api.seatgeek.com/2/events?q=' + search + '&client_id=NzA2MzY4MnwxNDg5NTE0NDA0Ljc1';
 
   request(queryString, function(error, res, body) {
     if(!error) {
-      console.log('in here');
-      console.log(body);
+
     }
   })
 }
