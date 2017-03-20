1864ed2612142cfd67dd3b746d5a59763deab5b2
Allen Guan <apguan@gmail.com>
2017-03-20 12:35:05 -0700

    added to seatgeekController

--- server/controllers/seatgeekController.js
+++ server/controllers/seatgeekController.js
@@ -1,16 +1,17 @@
 var app = require('../index.js');
 var request = require('request');
 
+//events?venue.city=
+
 var seatGeekGetter = function(searchParam, location, callback) {
-  search = searchParam.split(' ').join('+');
+  var artistName = searchParam.split(' ').join('+');
 
-  var queryString = 'https://api.seatgeek.com/2/events?q=' + search + '&client_id=NzA2MzY4MnwxNDg5NTE0NDA0Ljc1';
+  var queryString = 'https://api.seatgeek.com/2/performers?q=' + search + '&client_id=NzA2MzY4MnwxNDg5NTE0NDA0Ljc1';
 
-  request(queryString, function(error, res, body) {
-    if(!error) {
-
-    }
-  })
+  request.get(queryString)
+  .on('res', function(res) {
+    console.log()
+  }).pipe()
 }
 
 module.exports = {}
