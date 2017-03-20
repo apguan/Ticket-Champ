272fa14db52a883f536f7716bfa7e03e840a8c8b
Allen Guan <apguan@gmail.com>
2017-03-20 10:57:09 -0700

    changed spacing

--- server/controllers/seatgeekController.js
+++ server/controllers/seatgeekController.js
@@ -1,7 +1,13 @@
 var app = require('../index.js');
 
+//https://api.seatgeek.com/2/performers?q=lady+gaga&client_id=NzA2MzY4MnwxNDg5NTE0NDA0Ljc1
+//make a query request, then add the client_id to the end
+
+//pass the parameters in as array?
+
 var seatGeekGetter = function(callback) {
 
 }
 
-module.exports = {}
+module.exports = {
+}
