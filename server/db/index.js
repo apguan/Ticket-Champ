62c30ab7224181089597c9713691ac764c3dd0da
Aly-Tamboura <alytamboura7370@gmail.com>
2017-03-20 19:36:58 -0700

    added function to add query info to database

--- server/db/index.js
+++ server/db/index.js
@@ -13,4 +13,21 @@ connection.connect(function(err){
     console.log('the database is connected');
   }
 })
-module.exports = connection;
+
+var addTicketMasterToDataBase = function(dataObject) {
+	var params = [null, dataObject.venueName, dataObject.lowPrice, dataObject.averagePrice, dataObject.highPrice, dataObject.url, dataObject.date, dataObject.apiId, dataObject.city, dataObject.venueLocation, dataObject.state]
+	var queryString = 'INSERT INTO table_name (id, name, min, avg, max, webID, localDate, api, city, venueLocation, state ) VALUES ( ?,?,?,?,?,?,?,?,?,?,? );'
+	db.query(queryString, params, function(err, data) {
+		if(err) {
+			console.log('error saving to database')
+		} else {
+			console.log('saved query to data base')
+		}
+	})
+}
+module.exports = {
+  addTicketMasterToDataBase: addTicketMasterToDataBase,
+};
+
+
+
