7a7645438e526cf48e3ea3eab122eee782c761a5
Chris Chu <cchu008@gmail.com>
2017-03-23 18:00:04 -0700

    Tweaking grid view and changed styles.css well

--- client/src/components/Trending.jsx
+++ client/src/components/Trending.jsx
@@ -14,14 +14,23 @@ class Trending extends React.Component {
           <h1>Trending</h1>
         </div>
 
-        <div className="row">
-          <div className="col-md-12">
+        <div className="well well-sm">
+          <strong>Display</strong>
+          <div className="btn-group">
+            <a href="#" id="list" className="btn btn-default btn-sm"><span className="glyphicon glyphicon-th-list">
+            </span>List</a>
+            <a href="#" id="grid" className="btn btn-default btn-sm"><span
+                className="glyphicon glyphicon-th"></span>Grid</a>
+          </div>
+        </div>
 
+
+
+        <div id="products" className="row list-group">
            {this.props.trending.map( (item, key) =>
             <TrendingItem key={key} item={item} />
-        )}
+           )}
           </div>
-
         </div>
 
       </div>
