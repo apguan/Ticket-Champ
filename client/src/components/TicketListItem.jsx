87530c7d3f3b3f1633352706dc4b2e1d5b024bc7
Chris Chu <cchu008@gmail.com>
2017-03-24 22:32:28 -0700

    fix time bug render to show correct time

--- client/src/components/TicketListItem.jsx
+++ client/src/components/TicketListItem.jsx
@@ -3,18 +3,12 @@ var moment = require('moment');
 
 function TicketListItem (props) {
 
-  var time = props.searchItem.date.slice(11);
-  var dateObject = new Date(Date.parse(props.searchItem.date));
-  var dateReadable = dateObject.toDateString()
-  console.log('tick list item props: ', dateReadable);
-  var d = moment.utc(props.searchItem.date);
-  var tm = d.local().format('h:mm a');
+  var read = moment(props.searchItem.date).format('LLLL')
 
-//
 
   return (
           <tr className='tbl-row'>
-            <td>{dateReadable + ' ' + tm}</td>
+            <td>{read}</td>
             <td>{props.searchItem.venueName}</td>
             <td>{props.searchItem.venueLocation}</td>
             <td>{props.searchItem.averagePrice}</td>
