3d6289438d87d81f37e335abf252da171f9d6bbc
samzoll22 <sam.zoll22@gmail.com>
2017-03-26 22:50:38 -0700

    Implemented fluid animations and transitions

--- client/src/components/Trending.jsx
+++ client/src/components/Trending.jsx
@@ -11,17 +11,17 @@ class Trending extends React.Component {
       <div className="container">
 
           {this.props.clicked ? (
-            <div className="jumbotron loader">
-            <img className="img-center" src="https://s3-us-west-1.amazonaws.com/zollstorage/ticket-loader-lowRes.gif"/>
+            <div className="animated zoomIn jumbotron loader">
+            <img className="animated flip img-center" src="https://s3-us-west-1.amazonaws.com/zollstorage/ticket-loader-lowRes.gif"/>
             <h2 className="loadingText" data-text="Finding Ticket Prices…"> Finding Ticket Prices... </h2>
             </div>)
           :
           (
             <div>
-              <div className="jumbotron">
+              <div className="animated lightSpeedIn jumbotron">
                 <h1>Champ Tickets</h1>
               </div>
-              <div id="trending" className="animate slideInUp row list-group">
+              <div id="trending" className="animated slideInUp row list-group">
                  {this.props.trending.map( (item, key) =>
                   <TrendingItem key={key} item={item} search={this.props.search} changeClickState={this.props.clickState}/>
                   )}
